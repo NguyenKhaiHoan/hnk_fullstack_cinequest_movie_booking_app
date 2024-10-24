978c5e89e7190c88fefa37a8060488a22a72d496
tts_hoannk <tts_hoannk@viettelsoftware.com>
2024-10-24 17:49:51 +0700

    [Frontend] Clean branch frontend

--- backend/src/main/java/com/example/cinequest/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.example.cinequest.service;
-
-import org.thymeleaf.context.Context;
-
-public interface EmailService {
-    void sendVerificationEmail(String to, String subject, String text);
-
-    void sendVerificationEmailWithHtmlTemplate(String to, String subject, String templateName, Context context);
-}
